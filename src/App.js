3c210dcf6cb12f81c2c83d437811cbc4b55eb325
abdurrahman rajab <rajab-2@windowslive.com>
2020-03-22 17:28:38 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+      pis: []
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.pis.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(({people}) => this.setState({ pis: people }))
+
+  }
+
+}
+
+export default App
