82b12cbf5accbeab13ac5898770a977129023916
Halit Batur <halitfuatbatur@gmail.com>
2020-05-22 19:58:58 +0300

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            peopleInSpace: [],
+        }
+    }
+
+    // async componentDidMount() {
+    //     const data = await fetch('http://api.open-notify.org/astros.json');
+    //     const space = await data.json();
+    //     (async () => {
+    //         this.setState({
+    //             peopleInSpace: space.people,
+    //         })
+    //     })();
+
+
+    // }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    peopleInSpace: data.people
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.peopleInSpace.map(people => people.name)}
+            </div>
+        )
+    }
+}
+
+export default App
