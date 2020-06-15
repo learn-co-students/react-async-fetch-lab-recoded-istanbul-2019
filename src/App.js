60916a52e7f09f5d2abd4922ab502f5060987836
kamalbakour <kamal.bak92@gmail.com>
2020-06-15 23:29:59 +0300

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
-// create your App component here
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: 'failed',
+            number: '',
+            people: []
+        }
+    }
+    componentDidMount() {
+        this.fetchSample();
+    }
+    fetchSample = async () => {
+        const response = await fetch('http://api.open-notify.org/astros.json');
+        const { message, number, people } = await response.json();
+        return this.setState({
+            message: message,
+            number: number,
+            people: people
+        })
+    }
+    render() {
+        const { message, number, people } = this.state;
+        return (
+            <div>
+                {people.map((person, index) =>
+                    <span key={person + index}>Speciality: {person.craft} and Name: {person.name} <br /></span>)}
+
+            </div>
+        )
+    }
+}
+
+export default App;
